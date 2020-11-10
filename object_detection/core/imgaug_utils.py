48d4624a999097210e2f09beb11c858bc39e28ee
JinLuckyboy <34185570+JinLuckyboy@users.noreply.github.com>
2020-11-11 06:50:29 +0900

    Update imgaug_utils.py
    주석과 테스트 코드 삭제

--- object_detection/core/imgaug_utils.py
+++ object_detection/core/imgaug_utils.py
@@ -4,9 +4,6 @@ from tensorflow.python.framework.ops import EagerTensor
 import tensorflow as tf
 import numpy as np
 
-# import imgaug as ia
-# from matplotlib import pyplot as plt
-# import os.path
 augseq = iaa.Sequential([
     iaa.Crop(px=(0, 16)), # crop images from each side by 0 to 16px (randomly chosen)
     iaa.Fliplr(0.5), # horizontally flip 50% of the images
@@ -14,9 +11,6 @@ augseq = iaa.Sequential([
 ])
 
 def augment(image, boxes, labels):
-    # print("==========image.name(augment)", image.name)
-    # print("==========boxes", boxes)
-    # print("==========labels", labels)
     image_np = image.numpy() if type(image) == EagerTensor else image
     boxes_np = boxes.numpy() if type(boxes) == EagerTensor else boxes
     labels_np = labels.numpy() if type(labels) == EagerTensor else labels
@@ -41,68 +35,5 @@ def augment(image, boxes, labels):
     boxes_aug = np.array(boxes_aug)
     labels_aug = np.array(labels_aug)
     
-    # image_aug = image_aug.astype(np.uint8)
-    # print(image_aug)
-    # print(type(image_aug))
-    # if type(image_aug) == np.ndarray:
-        # i = 0
-        # file = f"output/output-{i}.jpg"
-        # while os.path.isfile(file):
-            # i += 1
-            # file = f"output/output-{i}.jpg"
-        # np.save(f"output/output{i}", image_aug)
-        # ia.imshow(image_aug)
-        # print(image_aug/255)
-        # plt.imshow(image_aug/255)
-        # plt.savefig(file)
-    # else:
-        # print(image_aug)
-        # print(type(image_aug))
-        # print("===============\n"*10)
-    
-    # image_aug = image_aug.astype(np.uint8)
-    
-    # image_aug = image_aug.reshape(-1)
-    # boxes_aug = boxes_aug.reshape(-1)
-    # labels_aug = labels_aug.reshape(-1)
-    
     merge = np.concatenate((image_aug, boxes_aug, labels_aug), axis=None)
     return merge
-    
-    # im = tf.get_default_graph().get_tensor_by_name("Squeeze_3:0")
-    # print(im)
-    # tf.assign(image, image_aug)
-    # tf.assign(boxes, boxes_aug)
-    # tf.assign(labels, labels_aug)
-    # return 0
-    
-    # image_aug = image_aug.astype(np.float32)
-    # boxes_aug = boxes_aug.astype(np.float32)
-    # labels_aug = labels_aug.astype(np.int32)
-    
-    # image_aug = [int(i) for i in image_aug.tobytes()]
-    # boxes_aug = [int(i) for i in boxes_aug.tobytes()]
-    # labels_aug = [int(i) for i in labels_aug.tobytes()]
-    
-    # dataset = tf.data.Dataset.from_tensor_slices([image_aug, boxes_aug, labels_aug])
-    
-    # print(f"image {image}{image.shape}/{image_aug}{image_aug.shape}")
-    # print(f"boxes {boxes}{boxes.shape}/{boxes_aug}{np.shape(boxes_aug)}")
-    # print(f"labels {labels}{labels.shape}/{labels_aug}{np.shape(labels_aug)}")
-    # print(f"[image_aug, boxes_aug, labels_aug] {[image_aug, boxes_aug, labels_aug]}")
-    # print(f"[image, boxes, labels] {[image, boxes, labels]}")
-    # return [(image_aug + b'\xff' + boxes_aug + b'\xff' + labels_aug)]
-    # return {image: image_aug, boxes: boxes_aug, labels: labels_aug}
-    # return [(image, boxes, labels)]
-    # return (image, boxes, labels)
-    # return dataset
-
-def test(image, boxes, labels, dataset):
-    # image = np.frombuffer(bytes(image), dtype=np.float32)
-    # boxes = np.frombuffer(bytes(boxes), dtype=np.float32)
-    # labels = np.frombuffer(bytes(labels), dtype=np.int32)
-    print(f"image: {image} {image.shape}")
-    print(f"boxes: {boxes}{boxes.shape}")
-    print(f"labels: {labels}{labels.shape}")
-    # print(f"result: {result}{result.shape}")
-    
