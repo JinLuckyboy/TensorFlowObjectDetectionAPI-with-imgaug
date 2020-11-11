38edc7488615f55fe78dcabf3758152aa317adee
JinLuckyboy <34185570+JinLuckyboy@users.noreply.github.com>
2020-11-12 03:06:06 +0900

    Update imgaug_utils.py
    random_order=True 추가
    debug image 코드 주석으로 추가

--- object_detection/core/imgaug_utils.py
+++ object_detection/core/imgaug_utils.py
@@ -8,7 +8,13 @@ augseq = iaa.Sequential([
     iaa.Crop(px=(0, 16)), # crop images from each side by 0 to 16px (randomly chosen)
     iaa.Fliplr(0.5), # horizontally flip 50% of the images
     iaa.GaussianBlur(sigma=(0, 3.0)) # blur images with a sigma of 0 to 3.0
+], random_order=True)
+""" # Debug image
+augseq = iaa.Sequential([
+    augseq,
+    iaa.SaveDebugImageEveryNBatches("debug_image", 1),
 ])
+"""
 
 def augment(image, boxes, labels):
     image_np = image.numpy().astype(np.uint8) if type(image) == EagerTensor else image
