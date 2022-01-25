90df139f0425cb4077ac28e125c9278a7bbeb53e
Santi Bacat <santibacat@gmail.com>
2022-01-25 18:53:05 +0100

    Fixed imgaug.py not being copied in script

--- install.sh
+++ install.sh
@@ -9,5 +9,6 @@ mv "${parent}/models/research/object_detection/protos/preprocessor.proto"       
 
 cp ./object_detection/builders/preprocessor_builder.py "${parent}/models/research/object_detection/builders/"
 cp ./object_detection/core/preprocessor.py "${parent}/models/research/object_detection/core/"
+cp ./object_detection/core/imgaug_utils.py "${parent}/models/research/object_detection/core/"
 cp ./object_detection/core/preprocessor_cache.py "${parent}/models/research/object_detection/core/"
 cp ./object_detection/protos/preprocessor.proto "${parent}/models/research/object_detection/protos/"
