eccbcb497c5dfb055f6ee161ba68755a82c24440
Santi Bacat <santibacat@gmail.com>
2022-01-25 18:44:20 +0100

    Fixed protos backup to allow proto builder

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 parent="$(dirname "$(pwd)")"
 
-mv "${parent}/models/research/object_detection/builders/preprocessor_builder.py" "${parent}/models/research/object_detection/builders/preprocessor_builder_orig.py"
-mv "${parent}/models/research/object_detection/core/preprocessor.py"             "${parent}/models/research/object_detection/core/preprocessor_orig.py"
-mv "${parent}/models/research/object_detection/core/preprocessor_cache.py"       "${parent}/models/research/object_detection/core/preprocessor_cache_orig.py"
-mv "${parent}/models/research/object_detection/protos/preprocessor.proto"        "${parent}/models/research/object_detection/protos/preprocessor_orig.proto"
+mv "${parent}/models/research/object_detection/builders/preprocessor_builder.py" "${parent}/models/research/object_detection/builders/preprocessor_builder.py.orig"
+mv "${parent}/models/research/object_detection/core/preprocessor.py"             "${parent}/models/research/object_detection/core/preprocessor.py.orig"
+mv "${parent}/models/research/object_detection/core/preprocessor_cache.py"       "${parent}/models/research/object_detection/core/preprocessor_cache.py.orig"
+mv "${parent}/models/research/object_detection/protos/preprocessor.proto"        "${parent}/models/research/object_detection/protos/preprocessor.proto.orig"
 
 cp ./object_detection/builders/preprocessor_builder.py "${parent}/models/research/object_detection/builders/"
 cp ./object_detection/core/preprocessor.py "${parent}/models/research/object_detection/core/"
